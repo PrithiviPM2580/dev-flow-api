b1a00bdd96123b26059f1a6feea3d90fd583cf74
Prithivi <prithivi@users.noreply.github.com>
2026-06-30 11:49:26 +0545

    chore(setup): initialize project with NestJS, Prisma, Docker, Swagger, and global config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Dev Flow API')
+    .setDescription(
+      'Dev Flow API  is a RESTful API built with NestJS, Prisma, and PostgreSQL. It provides endpoints for managing users, authentication, and other resources.',
+    )
+    .setVersion('1.0')
+    .addTag('Dev Flow API')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
